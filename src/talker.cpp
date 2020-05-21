b17c8c2855a5e185dc7012255b5a8559da091276
akhud78 <akhudiakov@gmail.com>
2020-05-21 18:13:50 +0300

    added parameter server example

--- src/talker.cpp
+++ src/talker.cpp
@@ -9,15 +9,20 @@
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "talker"); // name of the node
-  ros::NodeHandle n;  // main access point to communications with the ROS system
+  ros::NodeHandle nh;  // node handle
+  
   // chatter - topic name, 100 - size of the message queue
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 100);  
-  ros::Rate loop_rate(2); // 2Hz
+  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 100);  
+  ros::Rate loop_rate(1); // 1Hz
   int count = 0;
+  
+  std::string hello_name;
+  nh.param<std::string>("hello_name", hello_name, "world");
+  
   while (ros::ok()) {  // return false if SIGINT is received (Ctrl-C)
     std_msgs::String msg;
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "hello " << hello_name << " " << count;
     msg.data = ss.str();
     ROS_INFO("%s", msg.data.c_str()); // replacement for printf/cout
     chatter_pub.publish(msg);
